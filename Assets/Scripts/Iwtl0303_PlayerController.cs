0cc6a52c827431f1c6a133e5bf1fe7a6a80245db
Jeon Young Sang <iwtl0303@naver.com>
2026-01-31 11:07:03 +0900

    initial commit
    -Initialize repository with Unity project for KRAFTON Jungle Game Lab assignment

---
+++ Assets/Scripts/Iwtl0303_PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Iwtl0303_PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    public float speed = 10.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
+
+        
+    }
+
+
+}
