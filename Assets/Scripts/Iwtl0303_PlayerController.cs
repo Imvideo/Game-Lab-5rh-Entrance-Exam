00421ce0d7582187a50cc0d390aaee61cd17ec13
Jeon Young Sang <iwtl0303@naver.com>
2026-01-31 11:59:08 +0900

    feat: 순간이동 기능 추가
    -플레이어 순간이동 기능 구현

--- Assets/Scripts/Iwtl0303_PlayerController.cs
+++ Assets/Scripts/Iwtl0303_PlayerController.cs
@@ -5,7 +5,8 @@ public class Iwtl0303_PlayerController : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
     public float speed = 10.0f;
-
+    public float cooldown = 1.0f;
+    private float nextReadyTime = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,8 +21,22 @@ public class Iwtl0303_PlayerController : MonoBehaviour
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
 
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (Time.time >= nextReadyTime)
+            {
+                Teleport();
+                nextReadyTime = Time.time + cooldown;
+            }
+        }
     }
 
+    void Teleport() // 순간이동
+    {
+        Vector3 mouse = Input.mousePosition;
+        Vector3 world = Camera.main.ScreenToWorldPoint(mouse);
+
+        transform.position = new Vector3(world.x, world.y, transform.position.z);
+    }
 
 }
